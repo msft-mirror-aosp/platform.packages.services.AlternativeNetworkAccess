a26137bde8e37835b0880369b8574181e16146b1
Malcolm Chen <refuhoo@google.com>
2018-10-23 18:53:44 -0700

    Remove slotId in SubMgr#getOpportunisticSubscriptions.
    Bug: 117832560
    Test: build
    Change-Id: I28cb8b94c7b73d2b0921f8e6979fd2ddea0a4f9b
    Merged-In: I28cb8b94c7b73d2b0921f8e6979fd2ddea0a4f9b

--- src/com/android/ans/ANSProfileSelector.java
+++ src/com/android/ans/ANSProfileSelector.java
@@ -228,7 +228,7 @@ public class ANSProfileSelector {
 
     private int getSubId(String mcc, String mnc) {
         List<SubscriptionInfo> subscriptionInfos =
-                mSubscriptionManager.getOpportunisticSubscriptions(1);
+                mSubscriptionManager.getOpportunisticSubscriptions();
         for (SubscriptionInfo subscriptionInfo : subscriptionInfos) {
             if (TextUtils.equals(subscriptionInfo.getMccString(), mcc)
                     && TextUtils.equals(subscriptionInfo.getMncString(), mnc)) {
@@ -267,7 +267,7 @@ public class ANSProfileSelector {
 
     private void checkProfileUpdate() {
         List<SubscriptionInfo> subscriptionInfos =
-                mSubscriptionManager.getOpportunisticSubscriptions(1);
+                mSubscriptionManager.getOpportunisticSubscriptions();
         if (subscriptionInfos == null) {
             logDebug("received null subscription infos");
             return;
